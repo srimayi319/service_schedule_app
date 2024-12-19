51d0acb5867db34b2f218ea48ba4a7be150e4b21
srimayi319 <157908100+srimayi319@users.noreply.github.com>
2024-12-19 12:53:03 +0530

    firestore_view

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'auth/loginscreen.dart';
 import 'screens/products_page.dart';
 import 'screens/bookings.dart';
 import 'screens/services_page.dart';
+import 'screens/firestore_viewer.dart';
 import 'screens/profile_page.dart';
 import 'screens/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'screens/firestore_viewer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,23 +23,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.blue.shade900, // Primary seed color
-            brightness: Brightness.light,
-          ),
-          useMaterial3: true, // Optional for Material 3 style
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue.shade900, // Primary seed color
+          brightness: Brightness.light,
         ),
-        debugShowCheckedModeBanner: false,
+        useMaterial3: true, // Optional for Material 3 style
+      ),
+      debugShowCheckedModeBanner: false,
       initialRoute: '/', // Set initial route
       routes: {
         '/': (context) => const LoginScreen(), // Login Page
-
-        '/home':(context) =>  HomePage(),
-        '/products': (context) => const ProductsPage(), // Products Page
+        '/home': (context) => HomePage(),
+        '/products': (context) => ProductsPage(), // Products Page
         '/services': (context) => ServicePage(), // Services Page
         '/profile': (context) => const ProfilePage(),
-        '/bookings':(context)=> const BookingsPage(),
+        '/bookings': (context) => const BookingsPage(),
+        '/firestore': (context) => const FirestoreViewer(collectionName: 'users'), // Firestore Viewer Page
       },
     );
   }
